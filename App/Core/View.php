175393c79a4402d320c789859a1d71fbd58d686a
SaiKYouBoi <iliaselggames@gmail.com>
2026-01-14 00:23:56 +0100

    Refactor View class to include lexer registration for Twig templates

--- App/Core/View.php
+++ App/Core/View.php
@@ -3,6 +3,8 @@ namespace App\Core;
 
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Twig\Lexer;
+
 
 class View
 {
@@ -17,8 +19,22 @@ class View
                 'cache' => false, // enable in production
                 'debug' => true
             ]);
+
+            self::registerLexer();
         }
 
         echo self::$twig->render($template, $data);
     }
+
+    private static function registerLexer(): void
+    {
+        $lexer = new Lexer(self::$twig, [
+            'tag_block'    => ['{@', '@}'],   
+            'tag_variable' => ['{{', '}}'],  
+            'tag_comment'  => ['{#', '#}'],   
+        ]);
+
+        self::$twig->setLexer($lexer);
+    }
+    
 }
