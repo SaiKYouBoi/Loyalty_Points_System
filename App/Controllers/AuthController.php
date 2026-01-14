b6ce1806fdfcd563ca13abbb4ac7b53ab20b2beb
SaiKYouBoi <iliaselggames@gmail.com>
2026-01-14 18:33:09 +0100

    Refactor AuthController to improve session handling and add login validation

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Helpers\Session;
 use Database\Database;
+
 
 class AuthController extends Controller
 {
@@ -16,77 +18,130 @@ class AuthController extends Controller
         $this->view('register.view.twig');
     }
 
-    public function submitregister(){
+    public function submitregister()
+    {
         $err = false;
         $name = trim($_POST["name"] ?? '');
         $email = trim($_POST["email"] ?? '');
         $password = trim($_POST["password"] ?? '');
         $confirm_password = trim($_POST["password"] ?? '');
 
-        $this->validationRegister($err,$name,$email,$password,$confirm_password);
+        $this->validationRegister($err, $name, $email, $password, $confirm_password);
 
         if (!$err) {
             $query = "INSERT INTO users (name,email,password_hash) VALUES (:name,:email,:password_hash)";
             $stmt = Database::getInstance()->prepare($query);
-            if ($stmt->execute([
-                "name" => $name,
-                "email" => $email,
-                "password_hash" => password_hash($password,PASSWORD_BCRYPT)
-                ])) {
-                $_SESSION['success'] = "Registration successful.";
+            if (
+                $stmt->execute([
+                    "name" => $name,
+                    "email" => $email,
+                    "password_hash" => password_hash($password, PASSWORD_BCRYPT)
+                ])
+            ) {
+                Session::flash('success', 'Registration successful.');
                 header("Location: /login");
                 exit();
             }
-            
-            $_SESSION['error'] = "Registration failed. Please try again.";
+
+            Session::flash('error', 'Registration failed. Please try again.');
         }
 
         header("Location: /register");
         exit();
 
-    }   
+    }
 
-    public function validationRegister($err,$name,$email,$password,$confirm_password){
-        $_SESSION['name'] = htmlspecialchars($name, ENT_QUOTES);
-        $_SESSION['email'] = htmlspecialchars($email, ENT_QUOTES);
-        $_SESSION['password'] = htmlspecialchars($password, ENT_QUOTES);
-        $_SESSION['confirm_password'] = htmlspecialchars($confirm_password, ENT_QUOTES);
-        
-         if ($name === "") {
-            $_SESSION['firstName-err'] = "Name is required.";
+    public function validationRegister(&$err, $name, $email, $password, $confirm_password)
+    {
+        Session::flash('name', htmlspecialchars($name, ENT_QUOTES));
+        Session::flash('email', htmlspecialchars($email, ENT_QUOTES));
+        Session::flash('password', htmlspecialchars($password, ENT_QUOTES));
+        Session::flash('confirm_password', htmlspecialchars($confirm_password, ENT_QUOTES));
+
+        if ($name === "") {
+            Session::flash('name-err', 'Name is required.');
             $err = true;
         }
 
-
         if ($email === "" || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $_SESSION['email-err'] = "Email invalid.";
+            Session::flash('email-err', 'Email invalid.');
             $err = true;
-        }else{
+        } else {
             $query = "SELECT email FROM users WHERE email = ?";
             $stmt = Database::getInstance()->prepare($query);
             $stmt->execute([$email]);
 
             if ($stmt->rowCount() > 0) {
-                $_SESSION['email-err'] = "Email already exists.";
+                Session::flash('email-err', 'Email already exists.');
                 $err = true;
             }
         }
 
         if ($password === "") {
-            $_SESSION['password-err'] = "Password is required.";
+            Session::flash('password-err', 'Password is required.');
             $err = true;
         }
 
         if ($confirm_password === "") {
-            $_SESSION['confirm_password-err'] = "Password confirmation is required.";
+            Session::flash('confirm_password-err', 'Password confirmation is required.');
             $err = true;
         }
 
         if ($password !== $confirm_password) {
-            $_SESSION['confirm_password-err'] = "The passwords do not match.";
+            Session::flash('confirm_password-err', 'The passwords do not match.');
             $err = true;
         }
 
     }
 
+    public function submitLogin()
+    {
+        $err = false;
+        $email = trim($_POST["email"] ?? '');
+        $password = trim($_POST["password"] ?? '');
+
+        $this->validationLogin($err, $email, $password);
+        if (!$err) {
+            $query = "SELECT * FROM users WHERE email = ?";
+            $stmt = DataBase::getInstance()->prepare($query);
+            if ($stmt->execute([$email])) {
+                if ($stmt->rowCount() === 1) {
+                    $user = $stmt->fetch();
+                    if (password_verify($password, $user['password_hash'])) {
+                        unset($_SESSION['email'], $_SESSION['password']);
+                        Session::set('user_id', $user['id']);
+                        Session::flash('success', "Connexion réussie.");
+                        header("Location: /");
+                        exit();
+                    } else {
+                        Session::flash('error', "Email ou mot de passe incorrect.");
+                    }
+                } else {
+                    Session::flash('error', "Email ou mot de passe incorrect.");
+                }
+            } else {
+                Session::flash('error', "Une erreur est survenue. Veuillez réessayer plus tard.");
+            }
+        }
+
+        header("Location: /login");
+        exit();
+    }
+
+    public function validationLogin(&$err, $email, $password)
+    {
+
+        Session::flash("email", "htmlspecialchars($email, ENT_QUOTES)");
+        Session::flash("password", "htmlspecialchars($password, ENT_QUOTES)");
+
+        if ($email === "" || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            Session::flash("email-err", "Email invalide.");
+            $err = true;
+        }
+
+        if ($password === "") {
+            Session::flash("password-err", "Le mot de passe est obligatoire.");
+            $err = true;
+        }
+    }
 }
