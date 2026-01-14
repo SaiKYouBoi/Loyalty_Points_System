dbc4a0f421a7eac80a7ab01600ec037b3b376d63
SaiKYouBoi <iliaselggames@gmail.com>
2026-01-14 18:33:32 +0100

    Add missing helper functions inclusion in index.php

--- Public/index.php
+++ Public/index.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . "/../App/Helpers/functions.php";
 
 use App\Core\Router;
 
+
 require_once __DIR__ . '/../Routes/web.php';
 
+            
 Router::dispatch();
