9d2e574af3d2dc3ea503634f99ecabd2d9955efd
SaiKYouBoi <iliaselggames@gmail.com>
2026-01-19 16:34:26 +0100

    Add RewardsController to manage rewards and user points display

---
+++ App/Controllers/RewardsController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Models\Reward;
+use App\Models\User;
+
+class RewardsController extends Controller
+{
+    public function rewards()
+    {
+        $rewards = Reward::getAll();
+        $total = User::totalPoints();
+
+        $this->view('rewards.view.twig', [
+            'rewards' => $rewards,
+            'total_points' => $total['total_points']
+        ]);
+    }
+
+
+
+}
