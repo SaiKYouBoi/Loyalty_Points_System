8511e309749a9aef45fb65d010412e18f36be89f
SaiKYouBoi <iliaselggames@gmail.com>
2026-01-20 00:14:15 +0100

    Refactor RewardsController to instantiate User model for total points calculation

--- App/Controllers/RewardsController.php
+++ App/Controllers/RewardsController.php
@@ -6,18 +6,17 @@ use App\Models\Reward;
 use App\Models\User;
 
 class RewardsController extends Controller
-{
+{   
     public function rewards()
     {
         $rewards = Reward::getAll();
-        $total = User::totalPoints();
+        $total = (new User)->totalPoints();
 
         $this->view('rewards.view.twig', [
             'rewards' => $rewards,
             'total_points' => $total['total_points']
         ]);
     }
-
 
 
 }
