a85d7e83cad33490dc4f66b7d8ac127886815d2c
SaiKYouBoi <iliaselggames@gmail.com>
2026-01-19 16:35:14 +0100

    Fix reward route to use RewardsController and correct purchase route method

--- Routes/web.php
+++ Routes/web.php
@@ -4,6 +4,7 @@ use App\Core\Router;
 use App\Controllers\ShopController;
 use App\Controllers\AuthController;
 use App\Controllers\PointsController;
+use App\Controllers\RewardsController;
 
 Router::get('/', [ShopController::class, 'index']);
 
@@ -12,7 +13,7 @@ Router::get('/register', [AuthController::class, 'register']);
 Router::post('/register', [AuthController::class, 'submitregister']);
 Router::post('/login', [AuthController::class, 'submitLogin']);
 Router::post('/logout', [AuthController::class, 'logout']);
-Router::get('/reward', [ShopController::class, 'reward']);
+Router::get('/reward', [RewardsController::class, 'rewards']);
 Router::get('/transactions', [PointsController::class, 'transactions']);
 
 Router::get('/checkout', [ShopController::class, 'checkout']);
@@ -21,4 +22,4 @@ Router::get('/shop', [ShopController::class, 'shop']);
 Router::post('/cart/add', [ShopController::class, 'addToCart']);
 Router::post('/cart/empty', [ShopController::class, 'emptyCart']);
 
-Router::post('/purchase', [ShopController::class, 'emptyCart']);
+Router::post('/purchase', [PointsController::class, 'addPoints']);
